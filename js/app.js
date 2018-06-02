9d9226483a7dea2b674f397ace5847f2091f4078
Erin Miller <erinthered324@gmail.com>
2018-06-02 11:44:32 -0400

    Cards hide after two clicks

--- js/app.js
+++ js/app.js
@@ -37,10 +37,20 @@ function shuffle(array) {
  *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
  */
 
- var allCards = document.querySelectorAll('.card');
+var allCards = document.querySelectorAll('.card');
+var openCards = [];
 
  allCards.forEach(function(card) {
    card.addEventListener('click', function(e) {
-     card.classList.add('open', 'show');
+     if (openCards.length >= 2) {
+       setTimeout(function() {
+         openCards.forEach(function(card) {
+           card.classList.remove('open', 'show');
+         });
+       }, 1000);
+     } else {
+      openCards.push(card);
+      card.classList.add('open', 'show');
+     }
    });
  });
