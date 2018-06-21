e92249b99e7fbc281803d29a73fb94e53149d2b6
Erin Miller <erinthered324@gmail.com>
2018-06-21 16:57:08 -0400

    Adjust spacing and comments

--- js/app.js
+++ js/app.js
@@ -16,14 +16,8 @@ function generateCard(card) {
   return `<li class="card" data-card="${card}"><i class="fa ${card}"></i></li>`;
 }
 
-/*
- * Display the cards on the page
- *   - shuffle the list of cards using the provided "shuffle" method below
- *   - loop through each card and create its HTML
- *   - add each card's HTML to the page
- */
-
 // Shuffle function from http://stackoverflow.com/a/2450976
+
 function shuffle(array) {
     var currentIndex = array.length, temporaryValue, randomIndex;
 
@@ -38,17 +32,7 @@ function shuffle(array) {
     return array;
 }
 
-
-/*
- * set up the event listener for a card. If a card is clicked:
- *  - display the card's symbol (put this functionality in another function that you call from this one)
- *  - add the card to a *list* of "open" cards (put this functionality in another function that you call from this one)
- *  - if the list already has another card, check to see if the two cards match
- *    + if the cards do match, lock the cards in the open position (put this functionality in another function that you call from this one)
- *    + if the cards do not match, remove the cards from the list and hide the card's symbol (put this functionality in another function that you call from this one)
- *    + increment the move counter and display it on the page (put this functionality in another function that you call from this one)
- *    + if all cards have matched, display a message with the final score (put this functionality in another function that you call from this one)
- */
+//Start game
 
 function initGame() {
   var deck = document.querySelector('.deck');
@@ -59,6 +43,8 @@ function initGame() {
 }
 
 initGame();
+
+//Game functions
 
 var allCards = document.querySelectorAll('.card');
 var openCards = [];
@@ -87,9 +73,6 @@ var matchingCards = [];
              openCards = [];
            }, 1000);
          }
-         //if (matchingCards.length === 8) {
-           //stopTimer();
-         //}
          gameOver();
        }
      }
